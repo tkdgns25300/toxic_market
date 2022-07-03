5c499900e76d50a2b6b4c083614cbc7998f2ff72
hun <tkdgns25300@gmail.com>
2022-07-03 21:41:29 +0900

    refact: 불필요 주석 삭제

--- src/service/ImageUploadService.ts
+++ src/service/ImageUploadService.ts
@@ -11,7 +11,6 @@ export class ImageUploadService {
     if (!isValidBase64Image(paramObj)) {
       return new PageResObj({}, "유효한 Base64 이미지 값을 입력해주세요.", true);
     }
-
     // 이미지 업로드
     const result = await Promise.all(
       paramObj.map(async (el) => {
@@ -22,20 +21,10 @@ export class ImageUploadService {
   }
 
   async delete(paramObj: ImageUploadDto[]) {
-    /**
-     * TODO
-     * 삭제될 파일이 실존하는지 먼저 확인이 필요한가?
-     * s3 -> error를 리턴하는지, 아니면 컨트롤 가능한 형태의 데이터를 리턴하는지
-     * RDS 먼저 확인을 하거나 삭제요청을 보내고 콜백된 데이터로 판별이 가능
-     * delete 에서는 각 요소들 validate이후 Promise.all이용해 삭제 -> validate나 promise.all 하나라도 Error나면 PageResObj Error 리턴
-     * 추가로 s3.deleteObject의 - bucketName과 - key값에 이상한 값 넣어서 어떤 데이터(에러)가 리턴되는지 확인
-    */
-
     // 각 요소들 validate: 하나라도 유효하지 않을 경우 Error
     if (!await isValidURL(paramObj)) {
       return new PageResObj({}, "유효한 URL값을 입력해주세요.", true);
     }
-
     // 이미지 삭제
     await Promise.all(
       paramObj.map(async (el) => {
