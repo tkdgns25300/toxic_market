2ad860fbbdecd01e275eeefef4a94e57eaac0838
shamshod <realshamshod01@gmail.com>
2022-07-05 13:05:58 +0900

    feat: coin point exchange

--- src/controller/Exchange.ts
+++ src/controller/Exchange.ts
@@ -1,50 +1,50 @@
-// import {
-//   Body,
-//   JsonController,
-//   Res,
-//   UseBefore,
-//   Post,
-// } from "routing-controllers";
-// import { Response } from "express";
-// import { Inject, Service } from "typedi";
-// import { QueryFailedError } from "typeorm";
+import {
+  Body,
+  JsonController,
+  Res,
+  UseBefore,
+  Post,
+} from "routing-controllers";
+import { Response } from "express";
+import { Inject, Service } from "typedi";
+import { QueryFailedError } from "typeorm";
 
-// import { ExchangeService } from "../service/ExchangeService";
-// import { PageResObj } from "../api";
-// import { checkAccessToken } from "../middlewares/AuthMiddleware";
-// import { ExchangeDto } from "../dto";
+import { ExchangeService } from "../service/ExchangeService";
+import { PageResObj } from "../api";
+import { checkAccessToken } from "../middlewares/AuthMiddleware";
+import { ExchangeDto } from "../dto";
 
-// @Service()
-// @JsonController("/exchange")
-// export class ExchangeController {
-//   @Inject()
-//   exchangeService: ExchangeService;
+@Service()
+@JsonController("/exchange")
+export class ExchangeController {
+  @Inject()
+  exchangeService: ExchangeService;
 
-//   @Post("/coin")
-//   @UseBefore(checkAccessToken)
-//   public async toxToPoint(@Body() data: ExchangeDto, @Res() res: Response) {
-//     try {
-//       const { aud } = res.locals.jwtPayload;
-//       return await this.exchangeService.toxToPoint(data.amount, aud, null);
-//     } catch (err) {
-//       if (err instanceof QueryFailedError) {
-//         return new PageResObj({}, err.message, true);
-//       }
-//       return new PageResObj({}, err.message, true);
-//     }
-//   }
+  @Post("/coin")
+  @UseBefore(checkAccessToken)
+  public async toxToPoint(@Body() data: ExchangeDto, @Res() res: Response) {
+    try {
+      const { aud } = res.locals.jwtPayload;
+      return await this.exchangeService.toxToPoint(data.amount, aud, null);
+    } catch (err) {
+      if (err instanceof QueryFailedError) {
+        return new PageResObj({}, err.message, true);
+      }
+      return new PageResObj({}, err.message, true);
+    }
+  }
 
-//   @Post("/point")
-//   @UseBefore(checkAccessToken)
-//   public async pointToTox(@Body() data: ExchangeDto, @Res() res: Response) {
-//     try {
-//       const { aud } = res.locals.jwtPayload;
-//       return await this.exchangeService.pointToTox(data.amount, aud, null);
-//     } catch (err) {
-//       if (err instanceof QueryFailedError) {
-//         return new PageResObj({}, err.message, true);
-//       }
-//       return new PageResObj({}, err.message, true);
-//     }
-//   }
-// }
+  @Post("/point")
+  @UseBefore(checkAccessToken)
+  public async pointToTox(@Body() data: ExchangeDto, @Res() res: Response) {
+    try {
+      const { aud } = res.locals.jwtPayload;
+      return await this.exchangeService.pointToTox(data.amount, aud, null);
+    } catch (err) {
+      if (err instanceof QueryFailedError) {
+        return new PageResObj({}, err.message, true);
+      }
+      return new PageResObj({}, err.message, true);
+    }
+  }
+}
