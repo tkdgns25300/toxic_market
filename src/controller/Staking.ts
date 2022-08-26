b81248f025bbde458659e71352c245037b1945f6
hun <tkdgns25300@gmail.com>
2022-08-26 18:06:34 +0900

    feat: staking 필요 정보 정리

--- src/controller/Staking.ts
+++ src/controller/Staking.ts
@@ -72,3 +72,83 @@
 // : klaytn wallet key 
 // => 로그인에 필요함
 // => 절대 트랜잭션 서명에 사용 X
+
+# Klaytn 필수 정보
+
+클레이튼 지갑(메인넷)
+
+[https://wallet.klaytn.foundation/](https://wallet.klaytn.foundation/)
+
+클레이튼 지갑(바오밥)
+
+[https://baobab.wallet.klaytn.foundation/](https://baobab.wallet.klaytn.foundation/)
+
+클레이튼 공식 문서
+
+[https://ko.docs.klaytn.foundation/](https://ko.docs.klaytn.foundation/)
+
+클레이튼 API 공식 문서
+
+[https://docs.klaytnapi.com](https://docs.klaytnapi.com/tutorial/token-history-api/th-api-token)
+
+클레이튼 블록체인 네트워크의 전반적인 상황과 트랜잭션 또는 컨트랙트 정보를 볼 수 있는 서비스
+
+[https://scope.klaytn.com/](https://scope.klaytn.com/)
+
+[https://baobab.scope.klaytn.com/](https://baobab.scope.klaytn.com/)
+
+---
+
+### < wallet 기본 정보 >
+
+```json
+key store파일, password를 이용해 로그인 가능(klaytn wallet key로도 로그인 가능)
+=> 로그인 했다는 거는 private key에 접근 가능하다는 거고
+=> 이 부분은 트랜젝션 생성이 가능하다는 부분임.
+```
+
+```json
+0x6041b4636061525df35172d2f0c2fbd5554a6d4bd2e57f482f5f617c9318541b
+: private key => 트랜젝션 서명에 필요
+```
+
+```json
+0x6041b4636061525df35172d2f0c2fbd5554a6d4bd2e57f482f5f617c9318541b0x000xf9496b7e5989647ad47bcdbe3bd79e98fb836514
+: klaytn wallet key 
+=> 로그인에 필요함
+=> 절대 트랜잭션 서명에 사용 X
+```
+
+---
+
+### < 트랜잭션 수수료 >
+
+Transaction Fee : 트랜잭션 시 필요한 수수료(Gas Price * Gas Limit)
+
+Gas Price : 합의 노드에게 트랜잭션 처리해달라고 지불하는 돈(이더리움과 달리 항상 고정)
+
+Gas Limit : 트랜잭션을 처리하며 들어가는 Gas의 최대 한도 비용(트랜잭션 안에 무한 반복문이 있는 경우 Gas가 25000이 넘어가면 해당 작업 중지)
+
++. klay 단위 : [https://ko.docs.klaytn.foundation/klaytn/design/klaytn-native-coin-klay](https://ko.docs.klaytn.foundation/klaytn/design/klaytn-native-coin-klay)
+
++. **스마트 계약 함수를 통한 트랜젝션은 복잡성에 따라 Gas Limit이 바뀌기 때문에 Transaction Fee가 고정되어 있지 않음**
+
+---
+
+### < Klaytn IDE: 스마트 계약 작성 및 테스팅 환경  >
+
+[https://ide.klaytn.foundation/](https://ide.klaytn.foundation/)
+
+---
+
+**AccessKey ID**
+
+KASKQ16BGFGCW9RWI35226AX
+
+**Secret AccessKey**
+
+bx6M89gTan48YBeFGvnLkdGEMhwL6zKLqPIcOSYt
+
+**Authorization**
+
+Basic S0FTS1ExNkJHRkdDVzlSV0kzNTIyNkFYOmJ4Nk04OWdUYW40OFlCZUZHdm5Ma2RHRU1od0w2ektMcVBJY09TWXQ=
