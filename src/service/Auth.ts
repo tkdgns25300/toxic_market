a55297563834bdb296416664f2fdd0dfc35054ec
hun <tkdgns25300@gmail.com>
2022-09-14 16:23:45 +0900

    fix: signup logic 수정

--- src/service/Auth.ts
+++ src/service/Auth.ts
@@ -137,10 +137,10 @@ export class AuthService {
 
   async isHolder(owner: string) {
     const contracts = [
-      process.env.TOXIC_APE, //TOXIC APE
-      process.env.FOOLKATS, //FOOLKATS
-      process.env.SUCCUBUS, //SUCCUBUS
-      process.env.TOXIC_APE_SPECIAL, //TOXIC SPECIAL
+      process.env.TOXIC_APE,
+      // process.env.FOOLKATS,
+      // process.env.SUCCUBUS,
+      // process.env.TOXIC_APE_SPECIAL,
     ];
 
     // for each contract address make query if length is greater than 0 , then it is holder
