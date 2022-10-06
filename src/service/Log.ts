b7d6a5b0b53dbdf68716965c641c4f98ba1cd66a
hun <tkdgns25300@gmail.com>
2022-10-06 17:04:13 +0900

    feat: find all logs API - filter Log Data

--- src/service/Log.ts
+++ src/service/Log.ts
@@ -2,7 +2,7 @@ import { Service } from "typedi";
 import { InjectRepository } from "typeorm-typedi-extensions";
 
 import { LogQueryRepo } from "../repository/Log";
-import { Log } from "../entity";
+import { Log, User } from "../entity";
 import { PageResList } from "../api";
 import { LogSearchReq } from "../api/request/LogSearchReq";
 import { RaffleLogQueryRepo } from "../repository/RaffleLog";
@@ -31,45 +31,108 @@ export class LogService {
       buyer_id: string,
       seller_id: string,
       amount: number,
-      total_point: number
+      total_point: number,
+      created_at: string
     }
 
     // 각 마켓, 경매, 응모 로그 수집
-    const result = [];
+    let result = [];
     
     if (param.getSort === "" || param.getSort === LogClassification.PRODUCT) {
-      const productLogArr = await manager.query(
+      let productLog: logType[] = [];
+      const productLogRawArr = await manager.query(
         `SELECT * FROM log
          WHERE title like "%${param.getTitle}%"`
       );
       // 판매자, 구매자 아이디 솔팅
+      for (const data of productLogRawArr) {
+        const buyer = await manager.findOne(User, { public_address: data.buyer })
+        const seller = await manager.findOne(User, { public_address: data.seller })
+        const log: logType = {
+          classification: LogClassification.PRODUCT,
+          title: data.title,
+          buyer_id: buyer.id,
+          seller_id: seller.id,
+          amount: data.amount,
+          total_point: data.total_CF,
+          created_at: data.created_at
+        }
+        productLog.push(log)
+      }
+      productLog = productLog.filter(log => {
+        let isInclude = true;
+        if (param.getBuyerId !== "" && (log.buyer_id === null || (log.buyer_id && !log.buyer_id?.includes(param.getBuyerId)))) isInclude = false
+        else if (param.getSellerId !== "" && (log.seller_id === null || (log.seller_id && !log.seller_id?.includes(param.getSellerId)))) isInclude = false
+        return isInclude;
+      })
+      result = result.concat(productLog)
     }
 
     if (param.getSort === "" || param.getSort === LogClassification.AUCTION) {
-      const auctionLogArr = await manager.query(
-        `SELECT auction.price, auction.title, auction.created_at, auction.bidder, auction.bid, auction.creator_address, user.id FROM auction
-        JOIN user
-        ON auction.creator_address = user.public_address
-        WHERE auction.is_approved = "O"
-        AND auction.is_succeed = "O"
-        AND auction.end_at <= ?
-        AND auction.title like "%${param.getTitle}%"
-        AND user.id like "%${param.getSellerId}%" OR user.id IS NULL`,
+      let auctionLog: logType[] = [];
+      const auctionLogRawArr = await manager.query(
+        `SELECT auction.price, auction.title, auction.created_at, auction.bidder, auction.bid, auction.creator_address FROM auction
+        WHERE is_approved = "O"
+        AND is_succeed = "O"
+        AND end_at <= ?
+        AND title like "%${param.getTitle}%"`,
         [new Date()]
       );
-      // 구매자 아이디 솔팅
+      // 판매자, 구매자 아이디 솔팅
+      for (const data of auctionLogRawArr) {
+        const buyer = await manager.findOne(User, { public_address: data.bidder })
+        const seller = await manager.findOne(User, { public_address: data.creator_address })
+        const log: logType = {
+          classification: LogClassification.AUCTION,
+          title: data.title,
+          buyer_id: buyer.id,
+          seller_id: seller.id,
+          amount: 1,
+          total_point: data.bid,
+          created_at: data.created_at
+        }
+        auctionLog.push(log)
+      }
+      auctionLog = auctionLog.filter(log => {
+        let isInclude = true;
+        if (param.getBuyerId !== "" && (log.buyer_id === null || (log.buyer_id && !log.buyer_id?.includes(param.getBuyerId)))) isInclude = false
+        else if (param.getSellerId !== "" && (log.seller_id === null || (log.seller_id && !log.seller_id?.includes(param.getSellerId)))) isInclude = false
+        return isInclude;
+      })
+      result = result.concat(auctionLog)
     }
 
     if (param.getSort === "" || param.getSort === LogClassification.RAFFLE) {
-      const raffleLogArr = await manager.query(
+      let raffleLog: logType[] = [];
+      const raffleLogRawArr = await manager.query(
         `SELECT raffle_log.applicant, raffle_log.amount, raffle_log.created_at, raffle.price, raffle.title, raffle.creator_address FROM raffle_log
         JOIN raffle
         ON raffle_log.raffle_id = raffle.id
         WHERE raffle.title like "%${param.getTitle}%"`
       );
       // 판매자, 구매자 아이디 솔팅
+      for (const data of raffleLogRawArr) {
+        const buyer = await manager.findOne(User, { public_address: data.applicant })
+        const seller = await manager.findOne(User, { public_address: data.creator_address })
+        const log: logType = {
+          classification: LogClassification.RAFFLE,
+          title: data.title,
+          buyer_id: buyer.id,
+          seller_id: seller.id,
+          amount: data.amount,
+          total_point: data.amount * data.price,
+          created_at: data.created_at
+        }
+        raffleLog.push(log)
+      }
+      raffleLog = raffleLog.filter(log => {
+        let isInclude = true;
+        if (param.getBuyerId !== "" && (log.buyer_id === null || (log.buyer_id && !log.buyer_id?.includes(param.getBuyerId)))) isInclude = false
+        else if (param.getSellerId !== "" && (log.seller_id === null || (log.seller_id && !log.seller_id?.includes(param.getSellerId)))) isInclude = false
+        return isInclude;
+      })
+      result = result.concat(raffleLog)
     }
-
 
     // created_at 순으로 나열
 
