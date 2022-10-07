3bfa1cc896a0ed0a5c6ae13a4a2f18cbc9ad1264
hun <tkdgns25300@gmail.com>
2022-10-07 14:06:13 +0900

    update: get all user API 수정

--- src/api/request/UserSearchReq.ts
+++ src/api/request/UserSearchReq.ts
@@ -4,6 +4,8 @@ import { PageReq } from "./PageReq";
 export class UserSearchReq extends PageReq {
   name: string;
   public_address: string;
+  user_toxic_project: string;
+  user_catbotica_project: string;
 
   get getName() {
     return this.name ? this.name : "";
@@ -11,5 +13,13 @@ export class UserSearchReq extends PageReq {
 
   get getAddress() {
     return this.public_address ? this.public_address : "";
+  }
+
+  get getUserToxicProject() {
+    return this.user_toxic_project ? this.user_toxic_project : "";
+  }
+
+  get getUserCatboticaProject() {
+    return this.user_catbotica_project ? this.user_catbotica_project : "";
   }
 }
--- src/repository/User.ts
+++ src/repository/User.ts
@@ -12,25 +12,49 @@ export class UserQueryRepo extends BaseQueryRepo {
   }
 
   search(param: UserSearchReq): Promise<[Array<any>, number]> {
-    return createQueryBuilder("user")
-        .where(`name like :name`, {
-          name: `%${param.getName}%`,
-        })
-        .andWhere("public_address like :public_address", {public_address: `%${param.getAddress}%`})
-        .skip(param.getOffset())
-        .take(param.getLimit())
-        .getManyAndCount();
+    const builder = createQueryBuilder("user");
+
+    builder
+			.where(`name like :name`, {
+				name: `%${param.getName}%`,
+			})
+			.andWhere("public_address like :public_address", {
+				public_address: `%${param.getAddress}%`,
+			});
+
+    if (param.getUserToxicProject === 'O') {
+      builder.andWhere(`toxic_project = 'O'`)
+    }
+    if (param.getUserCatboticaProject === 'O') {
+      builder.andWhere(`catbotica_project = 'O'`)
+    }
+
+    builder.skip(param.getOffset()).take(param.getLimit());
+
+    return builder.getManyAndCount();
   }
 
   searchSeller(param: UserSearchReq): Promise<[Array<any>, number]> {
-    return createQueryBuilder("user")
-        .where(`is_seller = 'O'`)
-        .andWhere(`name like :name`, {
-          name: `%${param.getName}%`,
-        })
-        .andWhere("public_address like :public_address", {public_address: `%${param.getAddress}%`})
-        .skip(param.getOffset())
-        .take(param.getLimit())
-        .getManyAndCount();
+    const builder = createQueryBuilder("user");
+
+    builder
+      .where(`is_seller = 'O'`)
+			.andWhere(`name like :name`, {
+				name: `%${param.getName}%`,
+			})
+			.andWhere("public_address like :public_address", {
+				public_address: `%${param.getAddress}%`,
+			});
+
+    if (param.getUserToxicProject === 'O') {
+      builder.andWhere(`toxic_project = 'O'`)
+    }
+    if (param.getUserCatboticaProject === 'O') {
+      builder.andWhere(`catbotica_project = 'O'`)
+    }
+
+    builder.skip(param.getOffset()).take(param.getLimit());
+
+    return builder.getManyAndCount();
   }
 }
