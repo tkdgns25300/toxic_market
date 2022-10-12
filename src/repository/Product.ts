af7ffaabddba0c192e4cf619039cebb7c7e2dfe0
hun <tkdgns25300@gmail.com>
2022-10-12 12:58:37 +0900

    update: get all product 쿼리문 변경

--- src/repository/Product.ts
+++ src/repository/Product.ts
@@ -17,60 +17,51 @@ export class ProductQueryRepo extends BaseQueryRepo {
 		monthBeforeDate.setDate(monthBeforeDate.getDate() - 30);
 		const entityManager = getManager();
 
-		/**
-		 * 관계설정 후 Product에 유저의 닉네임 추가하는 방식(기존 데이터가 꼬일 우려가 있어 시도 어려움)
-		// 품절되지 않은 상품들
-		const notSoldoutProducts = await createQueryBuilder("product")
-		.leftJoinAndSelect("Product.user_address", "user")
-		.select([
-			"Product",
-			"user.public_address",
-			"user.name"
-		])
-		.where('amount != 0 OR amount IS NULL')
-		.andWhere('is_visible = :is_visible', {
-      is_visible: 'O'
-    })
-		.andWhere(`created_at >= :from_date`, {
-      from_date: monthBeforeDate
-    })
-		.orderBy('Product.created_at', 'DESC')
-		.skip(param.getOffset())
-		.take(param.getLimit())
-		.getManyAndCount();
-
-		// 품절 상품들
-		const soldoutProducts = await createQueryBuilder("product")
-		.leftJoinAndSelect("Product.user_address", "user")
-		.select([
-			"Product",
-			"user.public_address",
-			"user.name"
-		])
-		.where('amount = 0')
-		.andWhere('is_visible = :is_visible', {
-      is_visible: 'O'
-    })
-		.andWhere(`created_at >= :from_date`, {
-      from_date: monthBeforeDate
-    })
-		.orderBy('Product.created_at', 'DESC')
-		.skip(param.getOffset())
-		.take(param.getLimit())
-		.getManyAndCount();
-		*/
+		const orderAndLimitQuery = ` ORDER BY created_at DESC LIMIT 99999`;
+		let notSoldOutQuery = `
+		SELECT product.id, product.main_img_url, product.sub_img_url, product.price, product.amount, product.title, product.description, product.contact, product.created_at, product.user_address, product.is_visible
+		FROM product 
+		LEFT JOIN user 
+		ON product.user_address = user.public_address 
+		WHERE product.is_visible = "O" 
+		AND (product.amount != 0 OR product.amount IS NULL)
+		AND product.created_at >= "${monthBeforeDate.toISOString()}"`;
+		if (param.getUserToxicProject) notSoldOutQuery += ` AND user.toxic_project = '${param.getUserToxicProject}'`;
+		if (param.getUserCatboticaProject) notSoldOutQuery += ` AND user.catbotica_project = '${param.getUserCatboticaProject}'`;
+		notSoldOutQuery += orderAndLimitQuery;
+		
+		let soldOutQuery = `
+		SELECT product.id, product.main_img_url, product.sub_img_url, product.price, product.amount, product.title, product.description, product.contact, product.created_at, product.user_address, product.is_visible 
+		FROM product 
+		LEFT JOIN user 
+		ON product.user_address = user.public_address 
+		WHERE product.is_visible = "O" 
+		AND product.amount = 0
+		AND product.created_at >= "${monthBeforeDate.toISOString()}"`;
+		if (param.getUserToxicProject) soldOutQuery += ` AND user.toxic_project = '${param.getUserToxicProject}'`;
+		if (param.getUserCatboticaProject) soldOutQuery += ` AND user.catbotica_project = '${param.getUserCatboticaProject}'`;
+		soldOutQuery += orderAndLimitQuery;
 
     // Product
-		const result = await entityManager.query(
-			`(SELECT product.id, product.main_img_url, product.sub_img_url, product.price, product.amount, product.title, product.description, product.contact, product.created_at, product.user_address, product.is_visible FROM product LEFT JOIN user ON product.user_address = user.public_address WHERE product.is_visible = "O" AND (product.amount != 0 OR product.amount IS NULL) AND product.created_at >= ? AND user.toxic_project = ? AND user.catbotica_project = ? ORDER BY created_at DESC LIMIT 99999)
+		const result = await entityManager.query(`
+			(${notSoldOutQuery})
       UNION ALL
-	  (SELECT product.id, product.main_img_url, product.sub_img_url, product.price, product.amount, product.title, product.description, product.contact, product.created_at, product.user_address, product.is_visible FROM product LEFT JOIN user ON product.user_address = user.public_address WHERE product.is_visible = "O" AND product.amount = 0 AND product.created_at >= ? AND user.toxic_project = ? AND user.catbotica_project = ? ORDER BY created_at DESC LIMIT 99999)
+	  	(${soldOutQuery})
       LIMIT ${param.getLimit()}
-      OFFSET ${param.getOffset()};`
-		, [monthBeforeDate, param.getUserToxicProject, param.getUserCatboticaProject, monthBeforeDate, param.getUserToxicProject, param.getUserCatboticaProject]);
+      OFFSET ${param.getOffset()};
+		`);
 
     // Product 총 갯수
-		let totalCount = await entityManager.query(`SELECT COUNT(*) FROM product LEFT JOIN user ON product.user_address = user.public_address WHERE product.is_visible = 'O' AND product.created_at >= ? AND user.toxic_project = ? AND user.catbotica_project = ?`, [monthBeforeDate, param.getUserToxicProject, param.getUserCatboticaProject]);
+		let totalCountQuery = `
+			SELECT COUNT(*) FROM product
+			LEFT JOIN user
+			ON product.user_address = user.public_address
+			WHERE product.is_visible = 'O'
+			AND product.created_at >= "${monthBeforeDate.toISOString()}"`;
+		if (param.getUserToxicProject) totalCountQuery += ` AND user.toxic_project = '${param.getUserToxicProject}'`;
+		if (param.getUserCatboticaProject) totalCountQuery += ` AND user.catbotica_project = '${param.getUserCatboticaProject}'`;
+
+		let totalCount = await entityManager.query(totalCountQuery);
 		totalCount = Number(totalCount[0]["COUNT(*)"]);
 
 		return [result, totalCount];
